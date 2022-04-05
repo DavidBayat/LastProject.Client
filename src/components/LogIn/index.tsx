7bfc04c886f5db41fdc598dff14f2217e7a10063
tianbiao <45967570+AmushereTB@users.noreply.github.com>
2022-04-05 14:23:22 +0200

    changed google login position

--- src/components/LogIn/index.tsx
+++ src/components/LogIn/index.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import GoogleLogin from "react-google-login";
 import { useNavigate } from "react-router-dom";
+import "./Login.css"
 
 interface Props {
     setLoginData: Function
@@ -17,9 +18,10 @@ const Login = ({ setLoginData } : Props) => {
         alert(result)
     }
     return (
-        <div>
-            <h1 className="section-title">This is login page</h1>
+        <div className="section login">
+            <h1 className="section-title">Login With Google</h1>
             <GoogleLogin 
+                className="google-login"
                 clientId="348842152385-kdg8vkd6rnhvv07burv4pmune9ejalte.apps.googleusercontent.com"
                 buttonText="Log in with Google"
                 onSuccess={handleLogin}
